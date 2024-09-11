bd4218a9e738fa18a44155375357a9857fe7dc14
BelenHit <fernandblucie26@gmail.com>
2024-09-11 13:28:30 -0300

    Create script.js
    Interacción

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('btnComprar').addEventListener('click', function() {
+    alert('Gracias por tu interés. Estamos procesando tu compra de Cloro en Pastillas.');
+    // https://atom.bio/belltwilight71
+});
