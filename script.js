544aec10d67c98db05bb7af09ca06e19d52ab192
BelenHit <fernandblucie26@gmail.com>
2024-09-12 01:38:15 -0300

    Update script.js
    Hamburger menu function

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ document.getElementById('btnComprar').addEventListener('click', function() {
     alert('Gracias por tu interés. Estamos procesando tu compra de Cloro en Pastillas.');
     // https://atom.bio/belltwilight71
 });
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
