80004e7d585d6912a1772878ac3ca5ed83e714fe
NovaChemcorp <fernandblucie26@gmail.com>
2024-09-12 17:29:00 -0300

    Update script.js
    ELIMINE ALERTA

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.getElementById('btnComprar').addEventListener('click', function() {
-    alert('Gracias por tu interés. Estamos procesando tu compra de Cloro en Pastillas.');
     // https://atom.bio/belltwilight71
 });
 function toggleMenu() {
